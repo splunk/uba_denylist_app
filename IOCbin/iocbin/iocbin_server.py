ef44a423a22cd69552ebeacebc21e50d81bfdf47
Yuan Ling <yling@splunk.com>
2022-03-28 23:38:21 -0400

    refactor: set the port as an argument

---
+++ IOCbin/iocbin/iocbin_server.py
@@ -0,0 +1,20 @@
+import argparse
+from app import IOCbinServer
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-p", "--port", default=5000, help="Port to accept connections on", type=int
+    )
+
+    args = parser.parse_args()
+    print(f"[-] args: {args}")
+
+    iocbin_server = IOCbinServer(args.port)
+    iocbin_server.run_iocbin_server()
+
+if __name__ == "__main__":
+    main()
+
+
